2cee1eb483c698b4bdcbc8eaea0e69b0a4d77bb6
skqksjqk <82398740+skqksjqk@users.noreply.github.com>
2021-06-30 20:40:17 +0900

    Update views.py

--- board/views.py
+++ board/views.py
@@ -5,6 +5,9 @@ from django.http import HttpResponse
 import django.utils.crypto as crypt
 import bcrypt
 from django.utils import timezone
+from board.models import user
+import datetime
+
 
 # Create your views here.
 
@@ -24,7 +27,52 @@ def index(request) :
     
 
     '''
-
     d=rereply(replyid_id=1,cre_date=timezone.datetime.now(),rereply='''대댓글입니다3''',userid_id=6)
     d.save()
     return HttpResponse('오류가 안뜨면 저장이 완료된것')
+
+def register(request):      
+    if request.method == 'GET':
+        return render(request, 'login.html', {})
+
+    elif request.method=='POST':
+        userid = request.POST['user_id']
+        pw = request.POST['user_pw']
+        member = user(userid=bcrypt.hashpw(userid.encode('utf-8'),bcrypt.gensalt()), pw=bcrypt.hashpw(pw.encode('utf-8'),bcrypt.gensalt()),
+         signdate = timezone.datetime.now() ,
+          expiredate = timezone.datetime.now()+datetime.timedelta(days=650) ,
+          name = bcrypt.hashpw(userid.encode('utf-8'),bcrypt.gensalt()).decode()[:10],
+          dept = '국방부',
+          auth = '가입대기')
+        member.save()
+        return render(request, 'login.html', {})
+
+def updateinfo(request):
+    if request.method == 'GET':
+        return render(request, 'default.html', {})
+    return render(request, 'accountinfo.html', {})
+    
+    # elif request.method=='POST':
+
+
+
+
+
+
+
+
+
+
+# def login(request):
+#     if request.method == 'GET':
+#         form = LoginForm()
+#         return render(request, 'member/login.html', {'form': form})
+#     else:
+#         userid = request.POST['user_id']
+#         pw = request.POST['user_pw']
+#     try:
+#         User.objects.get(userid=userid, pw=pw)
+#     except User.DoesNotExist:
+#         return HttpResponse('로그인 실패')
+#     else:
+#         return HttpResponse('로그인 성공')
